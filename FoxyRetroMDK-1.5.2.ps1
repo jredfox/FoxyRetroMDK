2be5275e0576351012627c9695268831b271ba5e
jredfox <jredfox_@outlook.com>
2024-08-28 20:39:32 -0500

    WIP 1.4 support
    it appears all of 1.4x forge not only required different process slightly but forge MDK itself is f**** up. It doesn't compile without patching a .java.patch file in forge's 1.4x before running forge install

--- FoxyRetroMDK-1.5.2.ps1
+++ FoxyRetroMDK-1.5.2.ps1
@@ -2,11 +2,25 @@
 Add-Type -AssemblyName 'System.IO.Compression.FileSystem'
 
 #Change this mc release version between 1.1 through 1.5.2
-$mc_ver = "1.5"
+$mc_ver = "1.4.2"
+
 #Temp Files
 $mcp_dir = "$PSScriptRoot\MDK-$mc_ver"
 $temp = "$mcp_dir\tmp"
-#URLS 1.5.2
+#URL Start
+$argo_url = "https://web.archive.org/web/20160305211940id_/https://files.minecraftforge.net/fmllibs/argo-small-3.2.jar"
+$asm_url = "https://web.archive.org/web/20160305133607id_/https://files.minecraftforge.net/fmllibs/asm-all-4.1.jar"
+$bcprov_url = "https://web.archive.org/web/20130708220724id_/http://files.minecraftforge.net/fmllibs/bcprov-jdk15on-148.jar"
+$guava_url = "https://web.archive.org/web/20150324120717id_/https://files.minecraftforge.net/fmllibs/guava-14.0-rc3.jar"
+$scala_lib_url = "https://web.archive.org/web/20130708223654id_/http://files.minecraftforge.net/fmllibs/scala-library.jar"
+$jinput_url = "https://libraries.minecraft.net/net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar"
+$lwjgl_url = "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl/2.9.0/lwjgl-2.9.0.jar"
+$lwjgl_util_url = "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl_util/2.9.0/lwjgl_util-2.9.0.jar"
+$win_natives_url = "https://libraries.minecraft.net/net/java/jinput/jinput-platform/2.0.5/jinput-platform-2.0.5-natives-windows.jar"
+$win_natives_url2 = "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.0/lwjgl-platform-2.9.0-natives-windows.jar"
+$legacy_assets_url = "https://launchermeta.mojang.com/v1/packages/3d8e55480977e32acd9844e545177e69a52f594b/pre-1.6.json"
+$resources_url = "https://resources.download.minecraft.net/"
+#URLS that change based upon MC Version
 if ($mc_ver -eq "1.5.2")
 {
     $mcp_ver = "mcp751"
@@ -37,19 +51,22 @@ elseif ($mc_ver -eq "1.5")
     $mc_server_url = "https://launcher.mojang.com/v1/objects/aedad5159ef56d69c5bcf77ed141f53430af43c3/server.jar"
     $forge_lib_url = "https://web.archive.org/web/20160126150649id_/http://files.minecraftforge.net/fmllibs/fml_libs_dev15.zip"
 }
-
-$argo_url = "https://web.archive.org/web/20160305211940id_/https://files.minecraftforge.net/fmllibs/argo-small-3.2.jar"
-$asm_url = "https://web.archive.org/web/20160305133607id_/https://files.minecraftforge.net/fmllibs/asm-all-4.1.jar"
-$bcprov_url = "https://web.archive.org/web/20130708220724id_/http://files.minecraftforge.net/fmllibs/bcprov-jdk15on-148.jar"
-$guava_url = "https://web.archive.org/web/20150324120717id_/https://files.minecraftforge.net/fmllibs/guava-14.0-rc3.jar"
-$scala_lib_url = "https://web.archive.org/web/20130708223654id_/http://files.minecraftforge.net/fmllibs/scala-library.jar"
-$jinput_url = "https://libraries.minecraft.net/net/java/jinput/jinput/2.0.5/jinput-2.0.5.jar"
-$lwjgl_url = "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl/2.9.0/lwjgl-2.9.0.jar"
-$lwjgl_util_url = "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl_util/2.9.0/lwjgl_util-2.9.0.jar"
-$win_natives_url = "https://libraries.minecraft.net/net/java/jinput/jinput-platform/2.0.5/jinput-platform-2.0.5-natives-windows.jar"
-$win_natives_url2 = "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/2.9.0/lwjgl-platform-2.9.0-natives-windows.jar"
-$legacy_assets_url = "https://launchermeta.mojang.com/v1/packages/3d8e55480977e32acd9844e545177e69a52f594b/pre-1.6.json"
-$resources_url = "https://resources.download.minecraft.net/"
+elseif ($mc_ver -eq "1.4.2")
+{
+    $mcp_ver = "mcp719"
+    $mcp_url = "https://archive.org/download/minecraftcoderpack/minecraftcoderpack.zip/minecraftcoderpack/1.4.2/mcp719.zip"
+    $forge_url = "https://maven.minecraftforge.net/net/minecraftforge/forge/1.4.2-6.0.1.355/forge-1.4.2-6.0.1.355-src.zip"
+    $mc_url = "https://launcher.mojang.com/v1/objects/42d6744cfbbd2958f9e6688dd6e78d86d658d0d4/client.jar"
+    $mc_server_url = "https://launcher.mojang.com/v1/objects/5be700523a729bb78ef99206fb480a63dcd09825/server.jar"
+    $forge_lib_url = "https://web.archive.org/web/20130305145719if_/http://files.minecraftforge.net/fmllibs/fml_libs_dev.zip"
+    #Older then 1.5 Forge Uses Older Libraries
+    $argo_url = "https://web.archive.org/web/20130313100037if_/http://files.minecraftforge.net:80/fmllibs/argo-2.25.jar"
+    $asm_url = "https://web.archive.org/web/20130313081705if_/http://files.minecraftforge.net:80/fmllibs/asm-all-4.0.jar"
+    $bcprov_url = "https://web.archive.org/web/20130322004354if_/http://files.minecraftforge.net:80/fmllibs/bcprov-jdk15on-147.jar"
+    $guava_url = "https://web.archive.org/web/20130313081716if_/http://files.minecraftforge.net:80/fmllibs/guava-12.0.1.jar"
+    $scala_lib_url = ""
+    $mcp_srg_url = "" #MCP_SRG doesn't exist pre 1.5
+}
 #DIRS 1.5.2
 
 
@@ -81,13 +98,25 @@ Invoke-WebRequest -Uri "$forge_url" -OutFile "$temp/forge.zip"
 Invoke-WebRequest -Uri "$forge_lib_url" -OutFile "$temp/forge_lib.zip"
 [System.IO.Compression.ZipFile]::ExtractToDirectory("$temp/forge_lib.zip", "$mcp_dir/lib")
 
-#Download & Install Forge Runtime Libs
-Invoke-WebRequest -Uri "$argo_url" -OutFile "$mcp_dir/jars/lib/argo-small-3.2.jar"
-Invoke-WebRequest -Uri "$asm_url" -OutFile "$mcp_dir/jars/lib/asm-all-4.1.jar"
-Invoke-WebRequest -Uri "$bcprov_url" -OutFile "$mcp_dir/jars/lib/bcprov-jdk15on-148.jar"
-Invoke-WebRequest -Uri "$mcp_srg_url" -OutFile "$mcp_dir/jars/lib/deobfuscation_data_$mc_ver.zip"
-Invoke-WebRequest -Uri "$guava_url" -OutFile "$mcp_dir/jars/lib/guava-14.0-rc3.jar"
-Invoke-WebRequest -Uri "$scala_lib_url" -OutFile "$mcp_dir/jars/lib/scala-library.jar"
+#Download & Install Forge Runtime Libs if they Exist for this MC & Forge Version
+if ($argo_url -ne "") {
+    Invoke-WebRequest -Uri "$argo_url" -OutFile ("$mcp_dir/jars/lib/" + [System.IO.Path]::GetFileName("$argo_url"))
+}
+if ($asm_url -ne "") {
+    Invoke-WebRequest -Uri "$asm_url" -OutFile ("$mcp_dir/jars/lib/" + [System.IO.Path]::GetFileName("$asm_url"))
+}
+if ($bcprov_url -ne "") {
+    Invoke-WebRequest -Uri "$bcprov_url" -OutFile ("$mcp_dir/jars/lib/" + [System.IO.Path]::GetFileName("$bcprov_url"))
+}
+if ($mcp_srg_url -ne "") {
+    Invoke-WebRequest -Uri "$mcp_srg_url" -OutFile ("$mcp_dir/jars/lib/" + [System.IO.Path]::GetFileName("$mcp_srg_url"))
+}
+if ($guava_url -ne "") { 
+    Invoke-WebRequest -Uri "$guava_url" -OutFile ("$mcp_dir/jars/lib/" + [System.IO.Path]::GetFileName("$guava_url"))
+}
+if ($scala_lib_url -ne "") { 
+    Invoke-WebRequest -Uri "$scala_lib_url" -OutFile ("$mcp_dir/jars/lib/" + [System.IO.Path]::GetFileName("$scala_lib_url"))
+}
 
 #Download minecraft.jar & minecraft_server.jar and Install it
 Invoke-WebRequest -Uri "$mc_url" -OutFile "$mcp_dir/jars/bin/minecraft.jar"
