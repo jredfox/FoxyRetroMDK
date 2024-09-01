29f057dd9f4e18aaf1e4da84829ecf8e16747272
jredfox <jredfox_@outlook.com>
2024-09-01 00:58:50 -0500

    Create Uninstall-Java.sh

---
+++ Uninstall-Java.sh
@@ -0,0 +1,4 @@
+sudo rm -rf /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin  
+sudo rm -rf /Library/PreferencePanes/JavaControlPanel.prefPane  
+sudo rm -rf /Library/Application\ Support/Oracle/Java/ 
+sudo rm -rf /Library/Java/JavaVirtualMachines 
