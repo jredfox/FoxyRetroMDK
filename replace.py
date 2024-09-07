181c8e012550669aba119950130c154932fb7b71
jredfox <jredfox_@outlook.com>
2024-09-07 17:04:50 -0500

    fix lines

--- replace.py
+++ replace.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
         data = file.read()
 
     for i in range(2, len(sys.argv), 2):
-        old_str = sys.argv[i]
-        new_str = sys.argv[i + 1]
+        old_str = sys.argv[i].replace("\\n", "\n")
+        new_str = sys.argv[i + 1].replace("\\n", "\n")
         data = data.replace(old_str, new_str)
 
     with open(filename, 'w') as file:
