7b366086da49966acd9fe5e82e88a12016fd71fd
jredfox <jredfox_@outlook.com>
2024-09-07 13:02:10 -0500

    optimize code

--- replace.py
+++ replace.py
@@ -1,17 +1,14 @@
 import sys
 
-def replace_in_file(filename, old_str, new_str):
+if __name__ == "__main__":
+    filename = sys.argv[1]
     with open(filename, 'r') as file:
         data = file.read()
 
-    data = data.replace(old_str, new_str)
-
-    with open(filename, 'w') as file:
-        file.write(data)
-
-if __name__ == "__main__":
-    filename = sys.argv[1]
     for i in range(2, len(sys.argv), 2):
         old_str = sys.argv[i]
         new_str = sys.argv[i + 1]
-        replace_in_file(filename, old_str, new_str)
+        data = data.replace(old_str, new_str)
+
+    with open(filename, 'w') as file:
+        file.write(data)
