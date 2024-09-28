c2a48b7a8b5c4ca509e60077b8e317759a64d513
jredfox <jredfox_@outlook.com>
2024-09-27 19:29:02 -0500

    Update FoxyRetroMDK.cmd

--- FoxyRetroMDK.cmd
+++ FoxyRetroMDK.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM ## Get MC Version ##
 if "%mc_ver%" EQU "" (
-	set /p mc_ver="Enter Minecraft Version [1.1 - 1.6.4]: "
+    set /p mc_ver="Enter Minecraft Version [1.1 - 1.6.4]: "
 )
 REM ## Remove Quotes & Spaces ##
 set mc_ver=%mc_ver:"=%
