b08aca8d4efdfb969bda28d436da3a60d02700b2
jredfox <jredfox_@outlook.com>
2024-09-28 10:28:56 -0500

    WIP EOL II
    WIP EOL safety part II. removed < 1.5.2 for now

--- patchmdk.py
+++ patchmdk.py
@@ -30,11 +30,11 @@ else:
     )
 
 mcp_batch_patch = (
-    'REM ## FoxyForgeMDK JDK-8 START Patch ##\n'
-    'FOR /F "delims=" %%I IN (\'call "runtime\\bin\\python\\python_mcp.exe" "jdk-finder.py"\') DO SET "JAVA_DIR=%%I"\n'
-    'set "PATH=%JAVA_DIR%;%PATH%"\n'
-    'FOR %%I IN ("%JAVA_DIR%\..") DO SET "JAVA_HOME=%%~fI"\n'
-    'REM ## FoxyForgeMDK JDK-8 END Patch ##\n'
+    'REM ## FoxyForgeMDK JDK-8 START Patch ##\r\n'
+    'FOR /F "delims=" %%I IN (\'call "runtime\\bin\\python\\python_mcp.exe" "jdk-finder.py"\') DO SET "JAVA_DIR=%%I"\r\n'
+    'set "PATH=%JAVA_DIR%;%PATH%"\r\n'
+    'FOR %%I IN ("%JAVA_DIR%\..") DO SET "JAVA_HOME=%%~fI"\r\n'
+    'REM ## FoxyForgeMDK JDK-8 END Patch ##\r\n'
 )
 
 if __name__ == "__main__":
@@ -47,57 +47,30 @@ if __name__ == "__main__":
         isSh = file.endswith(".sh")
         if isSh or file.endswith(".bat") or file.endswith(".cmd"):
             with open(file, 'r') as f:
-                lines = f.readlines()
-            if isSh:
-                lines = [line.replace("python", "python2.7") for line in lines]
-            lines.insert(1, (mcp_sh_patch if isSh else mcp_batch_patch))
-            with open(file, 'w') as f:
-                f.writelines(lines)
+                lines = (f.read().replace("\r\n", "\n").replace("python", "python2.7").replace("\n", "\n" + mcp_sh_patch, 1) ) if isSh else (f.read().replace("\r\n", "\n").replace("\n", "\r\n").replace("\n", "\n" + mcp_batch_patch, 1))
+            with open(file, 'wb') as f:
+                f.write(lines)
                 print("Patching Path:" + file)
-
-    if not mcpInForge:
-        for file in glob.glob(os.path.normpath(mdk + "/forge/*")):
-            isSh = file.endswith(".sh")
-            if isSh or file.endswith(".bat") or file.endswith(".cmd"):
-                with open(file, 'r') as f:
-                    lines = f.readlines()
-                if isSh:
-                    lines = [line.replace("python", "python2.7") for line in lines]
-                lines.insert(1, (mcp_sh_patch.replace('cd "$mdk"\n', 'cd "$mdk"\nmdk="$(dirname "$mdk")"\n') if isSh else mcp_batch_patch.replace('"runtime\\bin\\python\\python_mcp.exe" "jdk-finder.py"', '"..\\runtime\\bin\\python\\python_mcp.exe" "..\\jdk-finder.py"') ))
-                with open(file, 'w') as f:
-                    f.writelines(lines)
-                    print("Patching Path:" + file)
-        for file in glob.glob(os.path.normpath(mdk + "/forge/fml/*")):
-            isSh = file.endswith(".sh")
-            if isSh or file.endswith(".bat") or file.endswith(".cmd"):
-                with open(file, 'r') as f:
-                    lines = f.readlines()
-                if isSh:
-                    lines = [line.replace("python", "python2.7") for line in lines]
-                lines.insert(1, (mcp_sh_patch.replace('cd "$mdk"\n', 'cd "$mdk"\nmdk="$(dirname "$mdk")"\nmdk="$(dirname "$mdk")"\n') if isSh else mcp_batch_patch.replace('"runtime\\bin\\python\\python_mcp.exe" "jdk-finder.py"', '"..\\..\\runtime\\bin\\python\\python_mcp.exe" "..\\..\\jdk-finder.py"')))
-                with open(file, 'w') as f:
-                    f.writelines(lines)
-                    print("Patching Path:" + file)
-    else:
+    if mcpInForge:
+        #Modify Patches based on Directory
+        str_mdk_sh = mcp_sh_patch.replace('bin_linux', 'mcp/bin_linux').replace('jdk-finder.py', 'mcp/jdk-finder.py')
+        str_mdk_cmd = mcp_batch_patch.replace('"runtime\\bin\\python\\python_mcp.exe" "jdk-finder.py"', '"mcp\\runtime\\bin\\python\\python_mcp.exe" "mcp\\jdk-finder.py"')
+        str_fml_sh = mcp_sh_patch.replace('bin_linux', 'mcp/bin_linux').replace('jdk-finder.py', 'mcp/jdk-finder.py').replace('cd "$mdk"\n', 'cd "$mdk"\nmdk="$(dirname "$mdk")"\n')
+        str_fml_cmd = mcp_batch_patch.replace('"runtime\\bin\\python\\python_mcp.exe" "jdk-finder.py"', '"..\\mcp\\runtime\\bin\\python\\python_mcp.exe" "..\\mcp\\jdk-finder.py"')
+        
         for file in glob.glob(os.path.normpath(mdk + "/*")):
             isSh = file.endswith(".sh")
             if isSh or file.endswith(".bat") or file.endswith(".cmd"):
                 with open(file, 'r') as f:
-                    lines = f.readlines()
-                if isSh:
-                    lines = [line.replace("python", "python2.7") for line in lines]
-                lines.insert(1, (mcp_sh_patch.replace('bin_linux', 'mcp/bin_linux').replace('jdk-finder.py', 'mcp/jdk-finder.py') if isSh else mcp_batch_patch.replace('"runtime\\bin\\python\\python_mcp.exe" "jdk-finder.py"', '"mcp\\runtime\\bin\\python\\python_mcp.exe" "mcp\\jdk-finder.py"')))
-                with open(file, 'w') as f:
-                    f.writelines(lines)
+                    lines = ( f.read().replace("\r\n", "\n").replace("python", "python2.7").replace("\n", "\n" + str_mdk_sh, 1) ) if isSh else f.read().replace("\r\n", "\n").replace("\n", "\r\n").replace("\n", "\n" + str_mdk_cmd, 1)
+                with open(file, 'wb') as f:
+                    f.write(lines)
                     print("Patching Path:" + file)
         for file in glob.glob(os.path.normpath(mdk + "/fml/*")):
             isSh = file.endswith(".sh")
             if isSh or file.endswith(".bat") or file.endswith(".cmd"):
                 with open(file, 'r') as f:
-                    lines = f.readlines()
-                if isSh:
-                    lines = [line.replace("python", "python2.7") for line in lines]
-                lines.insert(1, (mcp_sh_patch.replace('bin_linux', 'mcp/bin_linux').replace('jdk-finder.py', 'mcp/jdk-finder.py').replace('cd "$mdk"\n', 'cd "$mdk"\nmdk="$(dirname "$mdk")"\n') if isSh else mcp_batch_patch.replace('"runtime\\bin\\python\\python_mcp.exe" "jdk-finder.py"', '"..\\mcp\\runtime\\bin\\python\\python_mcp.exe" "..\\mcp\\jdk-finder.py"')))
-                with open(file, 'w') as f:
-                    f.writelines(lines)
+                    lines = ( f.read().replace("\r\n", "\n").replace("python", "python2.7").replace("\n", "\n" + str_fml_sh, 1) ) if isSh else f.read().replace("\r\n", "\n").replace("\n", "\r\n").replace("\n", "\n" + str_fml_cmd, 1)
+                with open(file, 'wb') as f:
+                    f.write(lines)
                     print("Patching Path:" + file)
