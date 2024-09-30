71b6a6fa5fae998a32cf570d61ecd1c8e72e6730
jredfox <jredfox_@outlook.com>
2024-09-29 20:25:46 -0500

    Update FoxyRetroMDK.sh

--- FoxyRetroMDK.sh
+++ FoxyRetroMDK.sh
@@ -228,6 +228,12 @@ function Patch-MDKPY {
         onesixflag="F"
     fi
     python2.7 "$SCRIPTPATH/patchmdk.py" "$mdk_dir" "$onesixflag"
+    
+    #Make all Files Executeable
+    if [[ "$isMac" == "true" ]]; then
+        xattr -r -d com.apple.quarantine "$mcp_dir/runtime/bin"
+    fi
+    chmod -R 777 "$mcp_dir/runtime/bin"
 }
 
 function Install-1.6x {
