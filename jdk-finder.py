ffae2979d71af0570c9fd5f1de0313934c9acf00
jredfox <jredfox_@outlook.com>
2025-11-09 23:22:34 -0600

    fix #2
    fix fake jdk installations resulted in false positives. This should make it so you can now have JDK 6 - 8 not required as the first in your actual path as long as it's a valid jdk installation containing the lib/libs folder

--- jdk-finder.py
+++ jdk-finder.py
@@ -31,8 +31,10 @@ def save(jdk_path, cache):
     sys.exit(0)
 
 def chk_jdk(jdk_path):
-    if jdk_path and jdk_path[1:].startswith(":\\Windows\\") or jdk_path[1:] == ":\\Windows":
-        return
+    #Skip Fake JDK Installations as we need the actual installation folder with the lib dir
+    parent = os.path.dirname(jdk_path)
+    if not os.path.isdir(os.path.join(parent, 'lib')) and not os.path.isdir(os.path.join(parent, 'libs')):
+            return
     if debug:
         print("checking:" + jdk_path)
     global jdk_ver
