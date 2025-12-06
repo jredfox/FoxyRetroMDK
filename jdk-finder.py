b74ea21cdb808943e1a32aeff9add74f17b3232d
jredfox <jredfox_@outlook.com>
2025-12-06 02:50:59 -0600

    Update jdk-finder.py

--- jdk-finder.py
+++ jdk-finder.py
@@ -103,10 +103,10 @@ def find_jdk():
             '/Library/Java/JavaVirtualMachines/*/Contents/Home/bin',
             '/System/Library/Java/JavaVirtualMachines/*/Contents/Home/bin',
             '/Applications/Java/JavaVirtualMachines/*/Contents/Home/bin',
-            '/usr/local/java/*/Contents/Home/bin',
-            '/opt/java/*/Contents/Home/bin',
             '/Library/PreferencePanes/JavaControlPanel.prefPane/Contents/Home/bin',
-            '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin'
+            '/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin',
+            '/usr/local/java/*/Contents/Home/bin',
+            '/opt/java/*/Contents/Home/bin'
         ]
         for path in mac_paths:
             for jdk_path in glob.glob(path):
