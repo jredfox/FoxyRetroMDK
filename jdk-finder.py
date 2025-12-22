368a7ee043d72b6634da7fb3e4fbc68e9cbe63a6
jredfox <jredfox_@outlook.com>
2025-12-21 23:21:12 -0600

    Update jdk-finder.py

--- jdk-finder.py
+++ jdk-finder.py
@@ -170,7 +170,7 @@ def find_jdk():
             for jdk_path in glob.glob(path):
                 if not jdk_path in lpaths:
                     lpaths.append(jdk_path)
-                    chk_jdk(jdk_path)
+                chk_jdk(jdk_path)
     
     if not pfirst:
         for directory in path_dirs:
