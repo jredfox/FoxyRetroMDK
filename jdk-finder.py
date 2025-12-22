382a0442f83478a8945cc4397afb0f71610c4611
jredfox <jredfox_@outlook.com>
2025-12-22 08:41:26 -0600

    fix derp

--- jdk-finder.py
+++ jdk-finder.py
@@ -170,7 +170,8 @@ def find_jdk():
             for jdk_path in glob.glob(path):
                 if not jdk_path in lpaths:
                     lpaths.append(jdk_path)
-                    chk_jdk(jdk_path)
+                    if os.path.isdir(jdk_path):
+                        chk_jdk(jdk_path)
     
     if not pfirst:
         for directory in path_dirs:
