8574d1c288d5540fbcd26e94149194d5380bf565
jredfox <jredfox_@outlook.com>
2025-12-24 00:46:48 -0600

    WIP resources

--- FoxyRetroMDK.ps1
+++ FoxyRetroMDK.ps1
@@ -165,7 +165,7 @@ $ProgressPreference = 'SilentlyContinue'
 try
 {
     $jsonFile = "$temp\assets.json"
-    Invoke-WebRequest -UserAgent "$Mozilla" -Uri "$JsonURL" -OutFile "$jsonFile"
+    Download -Uri "$JsonURL" -OutFile "$jsonFile"
     $jsonData = Get-Content -Path "$jsonFile" -Raw | ConvertFrom-Json
     $objects = $jsonData.objects
     foreach ($key in $objects.PSObject.Properties.Name)
@@ -176,7 +176,7 @@ try
         Write-Output "Downloading Resource URL:$resource"
         $rd = Split-Path "$resource_file" -Parent #build resource directory path
         New-Item -Path "$rd" -ItemType "directory" -Force | out-null #create resource directories if required
-        Invoke-WebRequest -UserAgent "$Mozilla" -Uri "$resource" -OutFile "$resource_file"
+        Download -Uri "$resource" -OutFile "$resource_file"
     }
 }
 catch
