5cda23cc24305c2b76491d9e54ba6c996874bc81
jredfox <jredfox_@outlook.com>
2025-12-24 18:42:44 -0600

    Update FoxyRetroMDK.ps1

--- FoxyRetroMDK.ps1
+++ FoxyRetroMDK.ps1
@@ -130,7 +130,7 @@ function Download {
     }
 }
 
-Download -Uri "https://httpbin.org/status/429" -OutFile "test.zip"
+#Download -Uri "https://httpbin.org/status/429" -OutFile "test.zip"
 
 #Author jredfox
 #This Download-Mediafire function is free to use, copy, and distribute
