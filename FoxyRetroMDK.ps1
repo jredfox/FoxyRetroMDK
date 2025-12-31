bac2615f652d23242daca932cc0809d6c421c3e3
jredfox <jredfox_@outlook.com>
2025-12-31 00:01:55 -0600

    Update FoxyRetroMDK.ps1

--- FoxyRetroMDK.ps1
+++ FoxyRetroMDK.ps1
@@ -17,8 +17,12 @@ if ($ps_ver -lt 5) {
     Write-Host "Legacy Powershell Detected $ps_ver Disabling Certificate & Enabling TLSv1.2 as the default!"
     Write-Warning "FoxyRetroMDK Requires Windows 7 KB3140245 or higher for HTTPS to Work!"
     Write-Warning "FoxyRetroMDK Requires .NET Framework 4.5"
-    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback =
+    [System.Net.Security.RemoteCertificateValidationCallback]{
+        param($sender, $cert, $chain, $errors)
+        return $true
+    }
 }
 
 #import C# zip tools
