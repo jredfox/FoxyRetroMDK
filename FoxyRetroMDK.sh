f615e167ecb9f64cada55360b483397a7a99ecbc
jredfox <jredfox_@outlook.com>
2026-01-01 19:15:55 -0600

    Update FoxyRetroMDK.sh

--- FoxyRetroMDK.sh
+++ FoxyRetroMDK.sh
@@ -74,6 +74,7 @@ function Download () {
                     break
                 fi
             done
+            #If not -k and Max Tries are 1 try again 1 more time with -k due to CERT issues
             if [[ "$MAX_TRIES" -eq 1 && "$ops" != *"-k"* ]]; then
                 ((MAX_TRIES++))
                 continue
