28697ce230c02ed33938f9719ae54d9862cac076
jredfox <jredfox_@outlook.com>
2026-01-11 08:27:05 -0600

    Update FoxyRetroMDK.ps1

--- FoxyRetroMDK.ps1
+++ FoxyRetroMDK.ps1
@@ -470,9 +470,9 @@ function DL-Natives
     )
 
     Download -Uri "$URL" -OutFile "$temp/$FileName.jar"
-    Download -Uri "$URL2" -OutFile "$temp/$FileName`2.jar"
+    Download -Uri "$URL2" -OutFile "$temp/${FileName}2.jar"
     [System.IO.Compression.ZipFile]::ExtractToDirectory("$temp\$FileName.jar", "$temp\natives")
-    [System.IO.Compression.ZipFile]::ExtractToDirectory("$temp\$FileName`2.jar", "$temp\natives")
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$temp\${FileName}2.jar", "$temp\natives")
     Remove-Item -Path "$temp\natives\META-INF" -Recurse -Force | out-null
     Get-ChildItem "$temp\natives\*.dylib" | Rename-Item -NewName { $_.Name -replace "\.dylib$",".jnilib" }
     [System.IO.Compression.ZipFile]::CreateFromDirectory("$temp\natives", "$mdk_dir\jars\bin\natives\$FileName.jar") # Re-Zips the natives and installs it to the correct
