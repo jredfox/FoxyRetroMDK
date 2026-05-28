1d947d167b079dc4a15cddb0d9e52631e2dade02
jredfox <jredfox_@outlook.com>
2026-05-28 00:25:44 -0500

    revert code

--- FoxyRetroMDK.sh
+++ FoxyRetroMDK.sh
@@ -171,7 +171,7 @@ function Check-LinuxDeps () {
     fi
 
     if [[ "$missing" == "T" ]]; then
-        echo "Try running bash Install-Linux-Deps.sh or manually installing these required packages: build-essential libssl-dev zlib1g-dev libncurses-dev libgdbm-dev liblzma-dev curl zip"
+        echo "Try running bash Install-Linux-Deps.sh or manually installing these required packages: build-essential libssl-dev zlib1g-dev libncurses-dev libgdbm-dev liblzma-dev curl"
         OnExit 1
     fi
 
