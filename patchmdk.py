e85facaf451e76343c5591505443974070132173
jredfox <jredfox_@outlook.com>
2026-05-31 15:35:50 -0500

    fix derps

--- patchmdk.py
+++ patchmdk.py
@@ -146,8 +146,8 @@ if __name__ == "__main__":
         #Make MC 1.1 - 1.2.5 Portabable when re-installing forge from non windows
         if os.getenv("patch_portability") == "T":
             print('Debug: Patching Portability for re-installing forge under forge/install.sh')
-            str_forge_sh = str_forge_sh.replace('mcp="$(dirname "$mcp")"\n', 'mcp="$(dirname "$mcp")"\n## Portability Patch Start ##\nchmod +x "$mcp"/*.sh\nchmod +x "$mcp/forge"/*.sh\nchmod +x "$mcp/forge/fml"/*.sh\n## Portability Patch End ##', 1)
-            str_forge_sh = str_forge_sh.replace('xattr -r -d com.apple.quarantine "$mcp/runtime/bin"\n', 'xattr -r -d com.apple.quarantine "$mcp/runtime/bin"\n## Portability Patch Start ##\nxattr -d com.apple.quarantine "$mcp/forge"/*.sh\nxattr -d com.apple.quarantine "$mcp/forge/fml"/*.sh\nxattr -d com.apple.quarantine "$mcp"/*.sh\n## Portability Patch End ##\n', 1)
+            str_forge_sh = str_forge_sh.replace('mcp="$(dirname "$mcp")"\n', 'mcp="$(dirname "$mcp")"\n## Portability Patch Start ##\nchmod +x "$mcp"/*.sh\nchmod +x "$mcp/forge"/*.sh\nchmod +x "$mcp/forge/fml"/*.sh\n## Portability Patch End ##\n', 1)
+            str_forge_sh = str_forge_sh.replace('xattr -r -d com.apple.quarantine "$mcp/runtime/bin"\n', 'xattr -r -d com.apple.quarantine "$mcp/runtime/bin"\n    ## Portability Patch Start ##\n    xattr -d com.apple.quarantine "$mcp/forge"/*.sh\n    xattr -d com.apple.quarantine "$mcp/forge/fml"/*.sh\n    xattr -d com.apple.quarantine "$mcp"/*.sh\n    ## Portability Patch End ##\n', 1)
         
         for file in glob.glob(os.path.normpath(mdk + "/forge/*")):
             isSh = file.endswith(".sh")
