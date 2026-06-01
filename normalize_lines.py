dc00d36e7a3dcf0dc0094f1a787234a87c1bae5c
jredfox <jredfox_@outlook.com>
2026-06-01 14:26:46 -0500

    WIP

---
+++ normalize_lines.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+if __name__ == "__main__":
+
+    EXTENSIONS = ('.sh', '.cfg', '.srg', '.java_', '.patch_', '.exc', '.cvs', '.py', ".py#", ".bck", '.py~')
+    script_path = os.path.realpath(__file__)
+    mcp = os.path.realpath(sys.argv[1])
+    winlf = False if (len(sys.argv) < 3) else (sys.argv[2][0].upper() == 'T')
+    printOnly = False if (len(sys.argv) < 4) else (sys.argv[3][0].upper() == 'T')
+    printOnly = True
+    
+    for root, dirs, files in os.walk(mcp):
+        for fname in files:
+            if fname.endswith(EXTENSIONS):
+                fpath = os.path.realpath(os.path.join(root, fname))
+                if fpath != script_path:
+                    try:
+                        with open(fpath, 'rb') as f:
+                            data = f.read()
+                        if not winlf:
+                            if '\r' in data:
+                                if not printOnly:
+                                    data = data.replace('\r\n', '\n').replace('\r', '\n')
+                                    with open(fpath, 'wb') as f:
+                                        f.write(data)
+                                print("normalized lines:" + fpath)
+                        elif not printOnly:
+                            data = data.replace('\r\n', '\n').replace('\r','\n').replace('\n', '\r\n')
+                            with open(fpath, 'wb') as f:
+                                f.write(data)
+                            print("normalized lines:" + fpath)
+                        else:
+                            print("normalized lines:" + fpath)
+                    except Exception as e:
+                        print("Error processing file:" + fpath + " " + str(e))
+    
