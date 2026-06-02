271a4d78329c689afdd54c11485e17f173860ad3
jredfox <jredfox_@outlook.com>
2026-06-02 11:24:50 -0500

    Update normalize_lines.py

--- normalize_lines.py
+++ normalize_lines.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     script_path = os.path.realpath(sys.argv[2])
     winlf = (os.name == 'nt') if (len(sys.argv) < 4) else (sys.argv[3][0].upper() == 'T')
     printOnly = False if (len(sys.argv) < 4) else (sys.argv[4][0].upper() == 'T')
-    EXTENSIONS = ('.sh', '.cfg')
     
     SCANDIRS = (mcp, os.path.join(mcp, "forge"), os.path.join(mcp, "forge/fml"), os.path.join(mcp, 'temp'), os.path.join(mcp, 'tmp'), os.path.join(mcp, 'conf'), os.path.join(mcp, 'forge/conf') )
     for root in SCANDIRS:
