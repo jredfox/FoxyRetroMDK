bc72bcd5cdce49f5c12569214807ffae63cf9735
jredfox <jredfox_@outlook.com>
2026-06-02 08:14:49 -0500

    optimize

--- normalize_lines.py
+++ normalize_lines.py
@@ -6,40 +6,31 @@ if __name__ == "__main__":
     script_path = os.path.realpath(__file__)
     mcp = os.path.realpath(sys.argv[1])
     winlf = False if (len(sys.argv) < 3) else (sys.argv[2][0].upper() == 'T')
-    printOnly = False if (len(sys.argv) < 4) else (sys.argv[3][0].upper() == 'T')
-    #EXTENSIONS = ('.sh', '.cfg', '.srg', '.exc', '.cvs', '.patch')
     EXTENSIONS = ('.sh', '.cfg')
-
-    for root, dirs, files in os.walk(mcp):
-        #Skip bin_linux
-        if os.path.basename(root) == 'bin_linux':
-            dirs[:] = []  # Don't recurse further
-            continue
-
-        for fname in files:
-            if fname.endswith(EXTENSIONS):
-                fpath = os.path.realpath(os.path.join(root, fname))
-                if fpath != script_path:
-                    try:
-                        with open(fpath, 'rb') as f:
-                            data = f.read()
-                        if not winlf:
-                            if '\r' in data:
-                                if not printOnly:
-                                    data = data.replace('\r\n', '\n').replace('\r', '\n')
-                                    #Normalize Paths for MC 1.1
-                                    if fname.endswith('.cfg'):
-                                        data = data.replace("\\", "/")
-                                    with open(fpath, 'wb') as f:
-                                        f.write(data)
-                                print("normalized lines:" + fpath)
-                        elif not printOnly:
-                            data = data.replace('\r\n', '\n').replace('\r','\n').replace('\n', '\r\n')
+    printOnly = True
+    
+    SCANDIRS = (mcp, os.path.join(mcp, "forge"), os.path.join(mcp, "forge/fml"), os.path.join(mcp, 'temp'), os.path.join(mcp, 'tmp'), os.path.join(mcp, 'conf'), os.path.join(mcp, 'forge/conf') )
+    for root in SCANDIRS:
+        if os.path.exists(root):
+            for fname in os.listdir(root):
+                cfg = fname.endswith('.cfg')
+                if cfg or fname.endswith('.sh'):
+                    fpath = os.path.realpath(os.path.join(root, fname))
+                    with open(fpath, 'rb') as f:
+                        data = f.read()
+                    if not winlf:
+                        if ('\r' in data or (cfg and '\\' in data)):
+                            data = data.replace('\r\n', '\n').replace('\r', '\n')
+                            if cfg:
+                                data = data.replace("\\", "/")
+                            if not printOnly:
+                                with open(fpath, 'wb') as f:
+                                    f.write(data)
+                            print("normalized lines:" + fpath)
+                    elif cfg:
+                        data = data.replace('\r\n', '\n').replace('\r', '\n').replace('\n', '\r\n').replace("\\", "/")
+                        if not printOnly:
                             with open(fpath, 'wb') as f:
                                 f.write(data)
-                            print("normalized lines:" + fpath)
-                        else:
-                            print("normalized lines:" + fpath)
-                    except Exception as e:
-                        print("Error processing file:" + fpath + " " + str(e))
+                        print("normalized lines:" + fpath)
     
