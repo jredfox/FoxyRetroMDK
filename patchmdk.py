10efeb6d308735a663bf689e079f065af801ccbf
jredfox <jredfox_@outlook.com>
2026-06-05 09:00:24 -0500

    Revert "Update patchmdk.py"
    This reverts commit 7b3e3eac6dc8e55577b250978866dc084f6ac6d4.

--- patchmdk.py
+++ patchmdk.py
@@ -145,7 +145,7 @@ if __name__ == "__main__":
         if os.getenv("patch_21") == "T":
             str_forge_sh = str_forge_sh.replace('## Foxy Retro MDK END ##\n', 'java -jar "$mcp/forge/PatchRenderPlayer.jar" "$mcp/forge"\n## Foxy Retro MDK END ##\n')
             str_fml_sh = str_fml_sh.replace('## Foxy Retro MDK END ##\n', 'java -jar "$mcp/forge/PatchRenderPlayer.jar" "$mcp/forge"\n## Foxy Retro MDK END ##\n')
-            str_forge_cmd = str_forge_cmd.replace('REM ## Foxy Retro MDK END ##\r\n', 'java -jar PatchRenderPlayer.jar "%~dp0\\.."\r\nREM ## Foxy Retro MDK END ##\r\n')
+            str_forge_cmd = str_forge_cmd.replace('REM ## Foxy Retro MDK END ##\r\n', 'java -jar PatchRenderPlayer.jar "%~dp0"\r\nREM ## Foxy Retro MDK END ##\r\n')
             str_fml_cmd = str_fml_cmd.replace('REM ## Foxy Retro MDK END ##\r\n', 'java -jar PatchRenderPlayer.jar "%~dp0\\.."\r\nREM ## Foxy Retro MDK END ##\r\n')
         
         #Make MC 1.1 - 1.2.5 Portabable when re-installing forge from non windows
