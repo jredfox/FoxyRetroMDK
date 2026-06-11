fda1e4f49b5afd5a52eafae5cdffb548ded4168a
jredfox <jredfox_@outlook.com>
2026-06-11 18:54:14 -0500

    latest

--- lwjglversionchanger.py
+++ lwjglversionchanger.py
@@ -54,6 +54,12 @@ if __name__ == "__main__":
         lwjgl_windows_sha1 = '4c517eca808522457dd95ee8fc1fbcdbb602efbe'
         lwjgl_macosx_sha1 = '2d12c83fdfbc04ecabf02c7bc8cc54d034f0daac'
         lwjgl_linux_sha1 = 'aa9aae879af8eb378e22cfc64db56ec2ca9a44d1'
+    elif lwjgl_ver == '2.9.4-nightly-20150209':
+        lwjgl_sha1 = '697517568c68e78ae0b4544145af031c81082dfe'
+        lwjgl_util_sha1 = 'd51a7c040a721d13efdfbd34f8b257b2df882ad0'
+        lwjgl_windows_sha1 = 'b84d5102b9dbfabfeb5e43c7e2828d98a7fc80e0'
+        lwjgl_macosx_sha1 = 'bcab850f8f487c3f4c4dbabde778bb82bd1a40ed'
+        lwjgl_linux_sha1 = '931074f46c795d2f7b30ed6395df5715cfd7675b'
 
     dir_bin = os.path.join(os.path.dirname(os.path.realpath(__file__)), "jars", "bin")
     dir_natives = os.path.join(dir_bin, 'natives')
