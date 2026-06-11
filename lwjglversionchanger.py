ad2d455c8d7409fa3fd7892f453772134c696dad
jredfox <jredfox_@outlook.com>
2026-06-11 17:55:39 -0500

    cleanup

--- lwjglversionchanger.py
+++ lwjglversionchanger.py
@@ -1,8 +1,36 @@
 import sys
 import os
 import shutil
-import urllib2
+import urllib
 import zipfile
+from contextlib import closing
+from hashlib import sha1
+
+def download_file(url, target, sha1, extract=False):
+    print('downloading:' + url)
+    urllib.urlretrieve(url, target)
+    downloaded_sha1 = get_sha1(target)
+    if (not sha1 is None) and downloaded_sha1 != sha1:
+        print('Download Failed Removing:' + target)
+        os.remove(target)
+        sys.exit(1)
+    if extract:
+        with zipfile.ZipFile(target, 'r') as zip_ref:
+            zip_ref.extractall(os.path.dirname(target))
+
+def get_sha1(file):
+    if not os.path.isfile(file):
+        return None
+    with closing(open(file, 'rb')) as fh:
+        return sha1(fh.read()).hexdigest()
+
+def del_dir(d):
+    if os.path.isdir(dir_natives):
+        shutil.rmtree(dir_natives)
+
+def del_file(file):
+    if(os.path.isfile(file)):
+        os.remove(file)
 
 if __name__ == "__main__":
     lwjgl_ver = sys.argv[1].lower().replace('"', '').replace("'", '').replace(' ', '')
@@ -22,36 +50,17 @@ if __name__ == "__main__":
     lwjgl_natives_linux_natives_jar = os.path.join(dir_natives, 'linux_natives.jar')
     
     #delete previous lwjgl
-    shutil.rmtree(dir_natives)
-    os.remove(lwjgl_jar)
-    os.remove(lwjgl_util_jar)
+    del_dir(dir_natives)
+    del_file(lwjgl_jar)
+    del_file(lwjgl_util_jar)
     os.makedirs(dir_natives)
 
     #download & install lwjgl
     lwjgl_url = 'https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl/' + lwjgl_ver + '/lwjgl-' + lwjgl_ver + '.jar'
-    lwjgl_util_url = 'https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl_util/' + lwjgl_ver + '/lwjgl_util-' + lwjgl_ver '.jar'
-    lwjgl_natives_base = "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/" + lwjgl_ver + "/lwjgl-platform-" + lwjgl_ver + "-natives-"
-    lwjgl_natives_windows = lwjgl_natives_base + "windows.jar"
-    lwjgl_natives_osx = lwjgl_natives_base + "osx.jar"
-    lwjgl_natives_linux = lwjgl_natives_base + "linux.jar"
-    download_extract(lwjgl_url, lwjgl_jar, None)
-    download_extract(lwjgl_util_url, lwjgl_util_jar, None)
-    download_extract(lwjgl_natives_base + "windows", lwjgl_natives_windows_natives_jar, None)
-    download_extract(lwjgl_natives_base + "osx", lwjgl_natives_macosx_natives_jar, None)
-    download_extract(lwjgl_natives_base + "linux", lwjgl_natives_linux_natives_jar, None)
-
-def download_extract(url, target, sha1):
-    urllib.urlretrieve(url, target)
-    downloaded_sha1 = get_sha1(target)
-    if downloaded_sha1 != sha1 and (not sha1 is None):
-        print('Download Failed Removing:' + target)
-        os.remove(target)
-        sys.exit(1)
-    with zipfile.ZipFile(target, 'r') as zip_ref:
-        zip_ref.extractall(os.path.dirname(target))
-
-def get_sha1(file):
-    if not os.path.isfile(file):
-        return None
-    with closing(open(file, 'rb')) as fh:
-        return sha1(fh.read()).hexdigest()
+    lwjgl_util_url = 'https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl_util/' + lwjgl_ver + '/lwjgl_util-' + lwjgl_ver + '.jar'
+    lwjgl_natives_base = "https://libraries.minecraft.net/org/lwjgl/lwjgl/lwjgl-platform/" + lwjgl_ver + '/lwjgl-platform-' + lwjgl_ver + "-natives-"
+    download_file(lwjgl_url, lwjgl_jar, None, False)
+    download_file(lwjgl_util_url, lwjgl_util_jar, None, False)
+    download_file(lwjgl_natives_base + "windows.jar", lwjgl_natives_windows_natives_jar, None, True)
+    download_file(lwjgl_natives_base + "osx.jar", lwjgl_natives_macosx_natives_jar, None, True)
+    download_file(lwjgl_natives_base + "linux.jar", lwjgl_natives_linux_natives_jar, None, True)
