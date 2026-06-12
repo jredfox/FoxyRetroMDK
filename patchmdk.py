5b507fc2406e849e6227b2660c7068b72bfebf8e
jredfox <jredfox_@outlook.com>
2026-06-12 03:39:37 -0500

    Update patchmdk.py

--- patchmdk.py
+++ patchmdk.py
@@ -70,7 +70,7 @@ lwjgl_version_changer_sh = (
     'isa="$(uname -m)"\n'
     'export PATH="$mcp/bin_linux/$isa/python2.7:$PATH"\n'
     'chmod -R 777 "$mcp/bin_linux"\n'
-    'python2.7 "lwjglversionchanger.py" "$1"'
+    'python2.7 "lwjglversionchanger.py" "$1"\n'
     '## Foxy Retro MDK END ##\n'
 )
 
