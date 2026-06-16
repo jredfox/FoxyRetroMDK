6c3b56a2c19005563b0b95b7292a8066a31f9d08
jredfox <jredfox_@outlook.com>
2026-06-16 00:44:44 -0500

    Update lwjglversionchanger.py

--- lwjglversionchanger.py
+++ lwjglversionchanger.py
@@ -115,9 +115,9 @@ def attatch_src(file, printSkip=False):
             f.write(lines)
         project_file = os.path.join(os.path.dirname(file), '.project')
         if os.path.isfile(project_file):
+            print('Patching: ' + project_file)
             with open(project_file, 'r') as f:
                 lines = f.read().replace('\r\n', '\n').replace('\\', '/').replace('    ', '\t').replace('   ', '\t')
-            print('Patching: ' + project_file)
             project_patch = '\t<link>\n\t\t\t<name>lib</name>\n\t\t\t<type>2</type>\n\t\t\t<locationURI>MCP_LOC/lib</locationURI>\n\t\t</link>\n\t'
             tag_start = lines.find('<linkedResources>')
             tag_end = lines.find('</linkedResources>')
