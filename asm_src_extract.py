391f29f79e9e6d2285a01eb38adce47f64dd7cc2
jredfox <jredfox_@outlook.com>
2026-06-18 03:50:57 -0500

    Update asm_src_extract.py

--- asm_src_extract.py
+++ asm_src_extract.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     mcp = sys.argv[1].lower().replace('"', '').replace("'", '')
     asm_tarball = os.path.join(mcp, 'lib', 'asm-4.1.tar.gz')
     if os.path.exists(asm_tarball):
-        print('extracting asm tarball')
+        print('Extracting asm-4.1.tar.gz')
         prefix = 'asm-4.1/src/'
         with tarfile.open(asm_tarball, "r:gz") as tar:
             with zipfile.ZipFile(os.path.join(mcp, 'lib', 'asm-all-4.1-source.zip'), 'w', zipfile.ZIP_DEFLATED) as zipf:
