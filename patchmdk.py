8ea0e5b7a7b579eb3d017f3ad3ad0a1ade6776a4
jredfox <jredfox_@outlook.com>
2026-06-24 14:50:27 -0500

    Update patchmdk.py

--- patchmdk.py
+++ patchmdk.py
@@ -191,7 +191,6 @@ if __name__ == "__main__":
         shutil.copyfile(os.path.join(script_dir, 'forge_install_prompt.py'), os.path.join(mdk, 'forge', 'forge_install_prompt.py') )
         eclipse_dir = os.path.join(mdk, 'forge', 'fml', 'eclipse')
         if not os.path.isdir(eclipse_dir):
-            print('ERR "eclipse" dir not found inside MDK/fml/eclipse!')
             eclipse_dir = os.path.join(mdk, 'eclipse')
         shutil.make_archive(os.path.join(mcp, 'runtime', 'eclipse'), 'zip', eclipse_dir)
         #Modify Patches based on Directory
