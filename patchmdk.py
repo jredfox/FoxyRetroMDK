c104b1c04759d4db69997a9bcad8a3d81e747f3d
jredfox <jredfox_@outlook.com>
2026-06-25 13:07:06 -0500

    1.1 - 1.2.4

--- patchmdk.py
+++ patchmdk.py
@@ -75,13 +75,13 @@ lwjgl_version_changer_sh = (
     '## Foxy Retro MDK END ##\n'
 )
 
-def patch_forge_install(lines, fml_script=False):
+def patch_forge_install(lines, isSh, fml_script=False):
     if isSh:
         lines = lines.replace('./cleanup.sh', './cleanup.sh -f')
         if applet_patch:
             lines = lines + '\npython2.7 "$mcp/forge/patch_applet.py" "$mcp"'
     else:
-        lines = lines.replace('runtime\\cleanup.py', 'runtime\\cleanup.py -f')
+        lines = lines.replace('runtime\\cleanup.py', 'runtime\\cleanup.py -f').replace('runtime/cleanup.py', 'runtime/cleanup.py -f')
         if applet_patch:
             lines = lines[:lines.rfind('pause')] + 'cd /D "%~dp0.."\r\nruntime\\bin\\python\\python_mcp forge\\patch_applet.py "."\r\npause'
     return lines
@@ -248,7 +248,7 @@ if __name__ == "__main__":
                 with open(file, 'r') as f:
                     lines = f.read()
                 lines = ( lines.replace("\r\n", "\n").replace("python", "python2.7").replace("\n", "\n" + str_forge_sh, 1) ) if isSh else lines.replace("\r\n", "\n").replace("\n", "\r\n").replace("\n", "\n" + str_forge_cmd, 1)
-                lines = patch_forge_install(lines)
+                lines = patch_forge_install(lines, isSh)
                 with open(file, 'wb') as f:
                     f.write(lines)
                 
@@ -259,7 +259,7 @@ if __name__ == "__main__":
                 with open(file, 'r') as f:
                     lines = f.read()
                 lines = ( lines.replace("\r\n", "\n").replace("python", "python2.7").replace("\n", "\n" + str_fml_sh, 1) ) if isSh else lines.replace("\r\n", "\n").replace("\n", "\r\n").replace("\n", "\n" + str_fml_cmd, 1)
-                lines = patch_forge_install(lines, True)
+                lines = patch_forge_install(lines, isSh, True)
                 with open(file, 'wb') as f:
                     f.write(lines)
         
