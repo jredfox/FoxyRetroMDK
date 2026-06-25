999925bd778556fddf04bf708f81b78b637ef16d
jredfox <jredfox_@outlook.com>
2026-06-25 07:40:56 -0500

    fix applet_patch
    fixed applet patch to patch the cleanup this should fix cleanup+decompile to re-install mcp and MC 1.1

--- patchmdk.py
+++ patchmdk.py
@@ -215,6 +215,14 @@ if __name__ == "__main__":
         
         #Patch MC 1.1 - 1.2.5 macOS graphical glitches on java 8!
         applet_patch = os.getenv("patch_applet") == "T"
+        if applet_patch:
+            cleanup_file = os.path.join(mdk, 'runtime', 'cleanup.py')
+            print("Patching Path:" + cleanup_file)
+            with open(cleanup_file, 'r') as f:
+                lines = f.read()
+            lines = lines.replace('\r\n', '\n').replace('commands = Commands(conffile)', '## Foxy Retro MDK Start ##\n    import shutil\n    dir_mdk = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))\n    shutil.copyfile(os.path.join(dir_mdk, \'forge\', \'conf\', \'patches\', \'Start.java\'), os.path.join(dir_mdk, \'conf\', \'patches\', \'Start.java\'))\n    ## Foxy Retro MDK End ##\n    commands = Commands(conffile)', 1)
+            with open(cleanup_file, 'wb') as f:
+                f.write(lines)
         
         for file in glob.glob(os.path.normpath(mdk + "/forge/*")):
             isSh = file.endswith(".sh")
