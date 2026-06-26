ee4d0ea02cb4efa0bef1910d129b45b487e03386
jredfox <jredfox_@outlook.com>
2026-06-26 07:25:41 -0500

    Update patchmdk.py

--- patchmdk.py
+++ patchmdk.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
                 lines = f.read()
             lines = lines.replace('headers = get_headers(url)', fml_py_patch, 1)
             if os.getenv("onesix_no_resources") == 'T':
-                lines = lines.replace('def download_assets(mcp_dir):\n', 'def download_assets(mcp_dir):\n    FOXY_RETRO_MDK_DISABLED = True\n    if FOXY_RETRO_MDK_DISABLED:\n        return\n')
+                lines = lines.replace('def download_assets(mcp_dir):\n', 'def download_assets(mcp_dir):\n    FOXY_RETRO_MDK_DISABLED = True\n    if FOXY_RETRO_MDK_DISABLED:\n        return\n', 1)
             with open(fmlpyf, 'wb') as f:
                 f.write(lines)
         
