5ab5bb785d7d1b66dd44ed4aadc0b073fa53cb1e
jredfox <jredfox_@outlook.com>
2026-06-26 12:35:14 -0500

    Update forge_install_prompt.py

--- forge_install_prompt.py
+++ forge_install_prompt.py
@@ -19,7 +19,7 @@ def main():
     #Check for Eclipse Lock
     sys.path.insert(0, mcp)
     from lwjglversionchanger import reset_eclipse, chk_eclipse
-    if installed_forge or os.path.isfile(os.path.join(mcp, 'eclipse', 'Client', '.classpath')):
+    if installed_forge or (not os.path.isfile(os.path.join(mcp, 'eclipse', 'Client', '.classpath'))):
         print('resetting eclipse...')
         reset_eclipse(mcp, os.path.join(dir_forge, 'fml'), os.path.join(mcp, 'eclipse'))
     #Create the installed file
