830bb6f6364195c6a0c9c50bd6d844424306b5c3
jredfox <jredfox_@outlook.com>
2026-06-30 15:55:36 -0500

    fix scriptpath
    fix the scriptpath sometimes being incorrect

--- FoxyRetroMDK.sh
+++ FoxyRetroMDK.sh
@@ -11,7 +11,7 @@ fi
 lwjgl_ver="$4"
 
 #Get Script's Absolute Path
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTPATH="$( cd -- "$(dirname "${BASH_SOURCE[0]:-$0}")" >/dev/null 2>&1 ; pwd -P )"
 isa="$(uname -m)"
 dir_bin="$SCRIPTPATH/bin/$isa"
 rp="$SCRIPTPATH/replace.py"
